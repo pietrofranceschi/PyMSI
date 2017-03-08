bugfix and add TIC calculation

build/lib.linux-x86_64-2.7/cmds/pymsi_cmd.py
@@ -1,7 +1,6 @@
 import click
 import pymsilib
-import numpy as np
-import os
+
 
 
 @click.group()
@@ -17,42 +16,14 @@ def cli():
 @click.argument('filename', type = str, nargs = -1)
 @click.option('--mzs', help = "mz of interest, comma separated",  type = str )
 @click.option('--tol', default = 1.0, help = "Tolerance for the extraction of the EIT", type = float)
-def getionimages(filename, mzs, tol):
+@click.option('--outdir', default = 'EIT', help = "Where to store the EITs", type = str)
+def getionimages(filename, mzs, tol, outdir):
     """
     Exract from a ImzML file a series of extracted ion images
     and store them into separate files
     """
-
-    with click.progressbar(filename) as bar:
-        for f in bar:
-            myEITs = pymsilib.getionimages(f,mzs,tol)
-            file_name, file_extension = os.path.splitext(f)
-            ## save the files
-            for i in range(0,len(myEITs)):
-                finalpath = os.path.join(file_name + '_mz_' + str(mz[i]) + '.csv')
-                np.savetxt(finalpath, EITs[i], delimiter=",")
-
-
-
-@cli.command()
-@click.argument('filename', type = str, nargs = -1)
-def getTIC(filename):
-    """
-    Exract from a ImzML file the TIC image
-    """
-
-    with click.progressbar(filename) as bar:
-        for f in bar:
-            myTICs = pymsilib.getTIC(f)
-            file_name, file_extension = os.path.splitext(f)
-            ## save the files
-            for i in range(0,len(myTICs)):
-                finalpath = os.path.join(file_name + '_TIC' + '.csv')
-                np.savetxt(finalpath, TICs[i], delimiter=",")
-
-
-
-
+    print(filename)
+    #chiesimsilib.getionimages(filename,mzs,tol,outdir)
 
 @cli.command()
 @click.option('--optical', help = "image file (read into with matplotlib)", type = str)
@@ -67,7 +38,7 @@ def hook(optical,EIT):
     optical  --  the name of the image file with the optical image
     EIT -- the file name of the csv containing the mz image
     '''
-    pymsilib.hook(optical,EIT)
+    chiesimsilib.hook(optical,EIT)
 
 
 @cli.command()
@@ -82,4 +53,4 @@ def transform(hooks,opticalimage,EIT,EITfolder,outdir):
     on the bases of a set of hook points. EIT images are resampled to match
     the size of the optical image
     '''
-    pymsilib.transform(hooks,opticalimage,EIT,EITfolder,outdir)
+    chiesimsilib.transform(hooks,opticalimage,EIT,EITfolder,outdir)

build/lib.linux-x86_64-2.7/pymsilib/_pymsilib.py
@@ -7,22 +7,16 @@ from skimage import transform as tf
 from skimage.color import rgb2gray
 from matplotlib.widgets import Button
 
-def getionimages(filename, mzs , tol = 1):
+def getionimages(filename, mzs , tol = 1, outdir = 'EIT'):
     """Extract from an ImzML file a series of
-    extracted ion images 
-   
-    :param filename:
-        the name of the ImzML file
-    :param mzs: 
-        a string with the target ions sebarated by commas
-    :param tol:
-        the m/z tolerance for extracting the trace (default = 1)
+    extracted ion images and save them as txt files
+    in a specific folder
 
-    Output:
-
-    :EITs:
-        a list of numpy matrices 
-
+    Keyword arguments:
+    filename -- the name of the ImzML file
+    mzs -- a string with the target ions sebarated by commas
+    tol -- the m/z tolerance for extracting the trace (default = 1)
+    outdir -- the directory where the EIT are stored (default = 'EIT')
     """
     p = ImzMLParser(filename)
     ## Extracct the ion images
@@ -30,29 +24,15 @@ def getionimages(filename, mzs , tol = 1):
     mz = [float(x.strip()) for x in mzs.split(',')]
     EITs  = [getionimage(p, x, tol) for x in mz]
 
-    return EITs
+    ## create the foder to save the EITs
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
-
-
-## TIC images
-
-def getTIC(filename, z=0):
-    """Generate a TIC image for a specific ImzML file and save it
-    as a csv file
-
-    :param filename:
-        the name of the ImzML file
-    :param z:
-        z Value if spectrogram is 3-dimensional.
-    """
-
-    p = ImzMLParser(filename)
-    im = np.zeros((p.imzmldict["max count of pixels y"], p.imzmldict["max count of pixels x"]))
-    for i, (x, y, z_) in enumerate(p.coordinates):
-        if z_ == z:
-            mzs, ints = map(lambda x: np.asarray(x), p.getspectrum(i))
-            im[y - 1, x - 1] = sum(ints)
-    return im
+    ## save the files
+    for i in range(0,len(EITs)):
+        finalpath = os.path.join(outdir,'mz_' + str(mz[i]) + '.csv')
+        print finalpath
+        np.savetxt(finalpath, EITs[i], delimiter=",")
 
 
 ## Alignment with the optical image -------------------------------------------